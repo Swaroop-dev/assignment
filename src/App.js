80f87b76630ddb2eb791cb943d1606359e0914b6
swaroop <swaroopaee98@gmail.com>
2021-03-21 14:59:36 +0530

    added a nav bar and navselect

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@
 import './App.css';
 import {useEffect,useState} from 'react';
 import React from "react-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+
+
 
 const App=()=>{
+  const useStyles = makeStyles({
+    root: {
+      width: 500,
+    },
+  });
+  
   const [data,setData]=useState([])
   const [error,setError]=useState({isError:"false",errormessage:""})
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
   const token="tTU3gFVUdP"
 
   useEffect(()=>{
@@ -57,6 +74,18 @@ const App=()=>{
                   
     { data.map((dat,index)=><p key={index}>{dat.awbno}</p>)} 
     {errorMessage()}
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
     </div>
   )
 
